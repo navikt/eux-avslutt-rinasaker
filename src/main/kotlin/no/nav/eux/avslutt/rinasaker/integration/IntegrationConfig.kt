00881435570916b7b396eace80403bf9718b9437
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2025-12-19 10:48:48 +0100

    25

--- src/main/kotlin/no/nav/eux/avslutt/rinasaker/integration/IntegrationConfig.kt
+++ src/main/kotlin/no/nav/eux/avslutt/rinasaker/integration/IntegrationConfig.kt
@@ -4,59 +4,45 @@ import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
-import org.springframework.boot.restclient.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpRequest
-import org.springframework.http.client.ClientHttpRequestExecution
-import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestClient
-import org.springframework.web.client.RestTemplate
 
 @EnableOAuth2Client(cacheEnabled = true)
 @Configuration
 class IntegrationConfig {
 
     @Bean
-    fun euxRinaTerminatorApiRestTemplate(components: RestTemplateComponents) =
+    fun euxRinaTerminatorApiRestClient(components: RestClientComponents) =
         components prefixBy "eux-rina-terminator-api"
 
     @Bean
-    fun restTemplateComponents(
-        restTemplateBuilder: RestTemplateBuilder,
+    fun restClientComponents(
+        restClientBuilder: RestClient.Builder,
         clientConfigurationProperties: ClientConfigurationProperties,
         oAuth2AccessTokenService: OAuth2AccessTokenService
-    ) = RestTemplateComponents(
-        restTemplateBuilder = restTemplateBuilder,
+    ) = RestClientComponents(
+        restClientBuilder = restClientBuilder,
         clientConfigurationProperties = clientConfigurationProperties,
         oAuth2AccessTokenService = oAuth2AccessTokenService
     )
 
-    infix fun RestTemplateComponents.prefixBy(appName: String): RestTemplate {
+    infix fun RestClientComponents.prefixBy(appName: String): RestClient {
         val clientProperties: ClientProperties = clientConfigurationProperties
             .registration["$appName-credentials"]
             ?: throw RuntimeException("could not find oauth2 client config for $appName-credentials")
-        return restTemplateBuilder
-            .additionalInterceptors(bearerTokenInterceptor(clientProperties, oAuth2AccessTokenService))
+        return restClientBuilder
+            .requestInterceptor { request, body, execution ->
+                val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
+                request.headers.setBearerAuth(response.access_token!!)
+                execution.execute(request, body)
+            }
             .build()
-    }
-
-    fun bearerTokenInterceptor(
-        clientProperties: ClientProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
-    ) = ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution ->
-        val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-        request.headers.setBearerAuth(response.access_token!!)
-        execution.execute(request, body)
     }
 }
 
-fun RestTemplate.restClient() = RestClient.create(this)
-fun RestTemplate.post() = restClient().post()
-fun RestTemplate.get() = restClient().get()
-
-data class RestTemplateComponents(
-    val restTemplateBuilder: RestTemplateBuilder,
+data class RestClientComponents(
+    val restClientBuilder: RestClient.Builder,
     val clientConfigurationProperties: ClientConfigurationProperties,
     val oAuth2AccessTokenService: OAuth2AccessTokenService
 )
