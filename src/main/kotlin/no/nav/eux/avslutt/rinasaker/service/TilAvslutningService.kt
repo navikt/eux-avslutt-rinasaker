c06b1946ffafe50f33acff54ca15e49668681d5c
Vegard <vegard@lundeberg.cc>
2026-02-23 12:18:52 +0100

    fix

--- src/main/kotlin/no/nav/eux/avslutt/rinasaker/service/TilAvslutningService.kt
+++ src/main/kotlin/no/nav/eux/avslutt/rinasaker/service/TilAvslutningService.kt
@@ -54,7 +54,7 @@ class TilAvslutningService(
             mottattSedExistsForAvslutning(dokumenter) -> rinasak avsluttMed scope
             sentSedExistsForAvslutning(dokumenter) -> rinasak avsluttMed scope
             uvirksomBucSkalAvsluttes(rinasak) -> rinasak avsluttMed scope
-//            opprettOppgave -> rinasak.lagOppgave() midlertidig tatt bort, avventer avklaring fra Siri/fag
+            opprettOppgave -> rinasak.lagOppgave()
         }
     }
 
